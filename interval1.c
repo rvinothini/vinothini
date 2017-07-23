225405b5a5980641e4a5fcf8b762158c8fadd7e1
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-23 10:05:07 +0530

    Rename intetval.c to interval1.c

---
+++ interval1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n=20,m=40;
+printf("Enter the number between the two intervals");
+for(i=n;i<=m;i++)
+{
+if(i%s==1)
+printf("It is a prime number");
+else
+printf("It is not an prime number");
+}
+}
